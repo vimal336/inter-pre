fc5889f9dcca3189e5aff7845b1d6526f1727d07
vimal336 <asvimal336@gmail.com>
2025-02-11 17:16:23 +0530

    useffect

--- src/Useeffecthook.jsx
+++ src/Useeffecthook.jsx
@@ -1,50 +1,50 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-// export const Useeffecthook  = () =>{
+export const Useeffecthook  = () =>{
 
-//   const [even, setEven] = useState(0);
-//   const [time, setTime] = useState(0);
+  const [even, setEven] = useState(0);
+  const [time, setTime] = useState(0);
 
-//   // No Dependencies
+  // No Dependencies
 
-//   // useEffect(()=>{
-//   //   console.log("No Dependency")
-//   // });
+  // useEffect(()=>{
+  //   console.log("No Dependency")
+  // });
 
-//   //Empty Dependency Array
+  //Empty Dependency Array
 
-//   //useEffect(()=>{
-//   //console.log("Empty Dependency Array")
-//   //},[]);
+  //useEffect(()=>{
+  //console.log("Empty Dependency Array")
+  //},[]);
 
   
-//   //State Dependency Array
+  //State Dependency Array
 
-//   useEffect(()=>{
-//   console.log("State Dependency Array")
-//   },[time]);
+  useEffect(()=>{
+  console.log("State Dependency Array")
+  },[time]);
 
-//   const addeven = () => {
-//     setEven((ev)=>(ev)+2)
-//   }
+  const addeven = () => {
+    setEven((ev)=>(ev)+2)
+  }
 
-//   const addodd = () => {
-//     setEven((o)=>(o)+1)
-//   }
+  const addodd = () => {
+    setEven((o)=>(o)+1)
+  }
 
-//   const addtime = () => {
-//     setTime((o)=>(o)+1)
-//   }
+  const addtime = () => {
+    setTime((o)=>(o)+1)
+  }
 
-//   return(
-//  <>
-//   <h1>Useffect Hook</h1>
-//   <p>{even}</p>
-//   <p>Clicked {time} Times</p>
-//   <button onClick={addodd}>Odd</button>
-//   <button onClick={addeven}>Even</button>
-//   <button onClick={addtime}>T</button>
-//  </>
-//   )
-// }
+  return(
+ <>
+  <h1>Useffect Hook</h1>
+  <p>{even}</p>
+  <p>Clicked {time} Times</p>
+  <button onClick={addodd}>Odd</button>
+  <button onClick={addeven}>Even</button>
+  <button onClick={addtime}>T</button>
+ </>
+  )
+}
 
