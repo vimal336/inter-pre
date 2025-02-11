4f59eafa1ea45ccf7b39e2aa31be595a0261ddb6
vimal336 <asvimal336@gmail.com>
2025-02-11 17:16:01 +0530

    useffect

--- src/Useeffecthook.jsx
+++ src/Useeffecthook.jsx
@@ -1,49 +1,71 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-export const Useeffecthook  = () =>{
+// export const Useeffecthook  = () =>{
 
-  const [even, setEven] = useState(0);
-  const [time, setTime] = useState(0);
+//   const [even, setEven] = useState(0);
+//   const [time, setTime] = useState(0);
 
-  // No Dependencies
+//   // No Dependencies
 
-  // useEffect(()=>{
-  //   console.log("No Dependency")
-  // });
+//   // useEffect(()=>{
+//   //   console.log("No Dependency")
+//   // });
 
-  //Empty Dependency Array
+//   //Empty Dependency Array
 
-  //useEffect(()=>{
-  //console.log("Empty Dependency Array")
-  //},[]);
+//   //useEffect(()=>{
+//   //console.log("Empty Dependency Array")
+//   //},[]);
 
   
-  //State Dependency Array
+//   //State Dependency Array
 
-  useEffect(()=>{
-  console.log("State Dependency Array")
-  },[time]);
+//   useEffect(()=>{
+//   console.log("State Dependency Array")
+//   },[time]);
 
-  const addeven = () => {
-    setEven((ev)=>(ev)+2)
-  }
+//   const addeven = () => {
+//     setEven((ev)=>(ev)+2)
+//   }
 
-  const addodd = () => {
-    setEven((o)=>(o)+1)
-  }
+//   const addodd = () => {
+//     setEven((o)=>(o)+1)
+//   }
 
-  const addtime = () => {
-    setTime((o)=>(o)+1)
-  }
+//   const addtime = () => {
+//     setTime((o)=>(o)+1)
+//   }
 
-  return(
- <>
-  <h1>Useffect Hook</h1>
-  <p>{even}</p>
-  <p>Clicked {time} Times</p>
-  <button onClick={addodd}>Odd</button>
-  <button onClick={addeven}>Even</button>
-  <button onClick={addtime}>T</button>
- </>
-  )
+//   return(
+//  <>
+//   <h1>Useffect Hook</h1>
+//   <p>{even}</p>
+//   <p>Clicked {time} Times</p>
+//   <button onClick={addodd}>Odd</button>
+//   <button onClick={addeven}>Even</button>
+//   <button onClick={addtime}>T</button>
+//  </>
+//   )
+// }
+
+import React, { useState, useEffect } from "react";
+
+const Useeffecthook = () => {
+    const [number, setNumber] = useState(0);
+
+    useEffect(()=>{
+          console.log("useEffect called");
+    },[])
+    const add = () =>{
+        setNumber(number+1)
+    }
+    return(
+        <>
+            <h1>Effect</h1>
+            <h1>{number}</h1>
+            <button onClick={add}>+</button>
+        </>
+    )
 }
+
+export default Useeffecthook;
