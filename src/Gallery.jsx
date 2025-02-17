80b3e3e355e57785fcad8b9ec7e147a8a7182e0c
vimal336 <asvimal336@gmail.com>
2025-02-17 13:05:04 +0530

    img responsive

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import { useState } from "react";
 import hooksImage from "../src/assets/images/hooks.jpg";
 import es6Image from "../src/assets/images/es6.jpg";
+import routesImage from "../src/assets/images/routes.jpg";
+import reduxImage from "../src/assets/images/redux.jpg";
+import hoistingImage from "../src/assets/images/hoisting.jpg";
+import uncontrolledImage from "../src/assets/images/uncontrolled.jpg";
+import PromiseImage from "../src/assets/images/promise.jpg";
 
-const images = [hooksImage, es6Image];
+const images = [hooksImage, es6Image, PromiseImage];
 
 const ImageGallery = () => {
+  const [fullScreenImage, setFullScreenImage] = useState(null);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-4">
       {images.map((src, index) => (
@@ -12,10 +19,23 @@ const ImageGallery = () => {
           <img
             src={src}
             alt={`Gallery Image ${index + 1}`}
-            className="w-full h-auto object-cover hover:scale-105 transition-transform duration-300"
+            className="w-full h-auto object-cover hover:scale-105 transition-transform duration-300 cursor-pointer"
+            onClick={() => setFullScreenImage(src)}
           />
         </div>
       ))}
+
+      {fullScreenImage && (
+        <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
+          <button
+            className="absolute top-4 right-4 bg-red-500 text-black px-4 py-2 rounded"
+            onClick={() => setFullScreenImage(null)}
+          >
+            X
+          </button>
+          <img src={fullScreenImage} alt="Full Screen" className="max-w-full max-h-full" />
+        </div>
+      )}
     </div>
   );
 };
