8a5a8b45a4a90f581a91198d4af787b157eba183
vimal336 <asvimal336@gmail.com>
2025-02-17 12:58:31 +0530

    img responsive

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,20 +1,18 @@
+import React from "react";
 import hooksImage from "../src/assets/images/hooks.jpg";
+import es6Image from "../src/assets/images/es6.jpg";
 
-
-const images = [hooksImage];
-
-
-console.log(images)
+const images = [hooksImage, es6Image];
 
 const ImageGallery = () => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-4">
       {images.map((src, index) => (
         <div key={index} className="overflow-hidden rounded-lg shadow-lg">
           <img
             src={src}
             alt={`Gallery Image ${index + 1}`}
-            className="w-full h-40 object-cover hover:scale-105 transition-transform duration-300"
+            className="w-full h-auto object-cover hover:scale-105 transition-transform duration-300"
           />
         </div>
       ))}
