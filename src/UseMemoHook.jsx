c5e0b3b4dee5777814e8663434e0af42052162ba
vimal336 <asvimal336@gmail.com>
2025-02-20 15:30:55 +0530

    usememo

--- src/UseMemoHook.jsx
+++ src/UseMemoHook.jsx
@@ -1,43 +1,9 @@
-// import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 
-// export const UseMemoHook = () => {
-//   const [number, setNumber] = useState(0);
-//   const [dark, SetDark] = useState(false);
+export const UseMemoHook = () => {
 
-  
-//   //const doubleNumber = slowFunction(number);
+  //const doubleNumber = slowFunction(number);
 
-//   const doubleNumber = useMemo(()=>{
-//     return slowFunction(number)
-//   },[number])
-
-//   const themeStyle = {
-//     backgroundColor: dark ?"white":"black",
-//     color: dark ?"black":"white"
-// }
-
-//   return (
-//     <>
-//       <p>UseMemoHook</p>
-//       <input type="number" value={number}
-//       onChange={(e)=> setNumber(e.target.value)}
-//       />
-//      <button onClick={() => SetDark((curr)=>!curr)}>Toggle Button</button>
-//      <div style={themeStyle}>{doubleNumber}</div>
-//     </>
-//   );
-// };
-
-// function slowFunction(num){
-//     console.log("slow function")
-//     for(let i=0; i<=1000000000; i++){}
-//     return num * 2
-// }
-
-
-import React, { useState, useMemo } from "react";
-
-const MemoExample = () => {
   const [count, setCount] = useState(0);
   const [toggle, setToggle] = useState(false);
 
@@ -59,4 +25,5 @@ const MemoExample = () => {
   );
 };
 
-export default MemoExample;
+
+
