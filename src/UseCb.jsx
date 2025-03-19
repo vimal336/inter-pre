8939a3273e1b6c9db223b39e567c9506524c734a
vimal336 <asvimal336@gmail.com>
2025-03-19 20:02:47 +0530

    project

--- src/UseCb.jsx
+++ src/UseCb.jsx
@@ -49,7 +49,7 @@ return (
           className="bg-red-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-red-600 transition"
           onClick={addTodo}
         >
-          ADD +
+          ADD 
         </button>
       </div>
 
