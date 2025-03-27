a3506bd28d5fbddbdd2ceca805b14a129d48aa4e
vimal336 <asvimal336@gmail.com>
2025-03-27 15:50:22 +0530

    problem solving

--- src/Challenge/Mytask.jsx
+++ src/Challenge/Mytask.jsx
@@ -153,7 +153,7 @@ const App = () => {
         </thead>
         {
             data.map((user,i)=>(
-              <tbody key={i} className="bg-gray-400">
+              <tbody key={i} className="bg-blue-400">
             <tr className="text-center">
             <td className="p-2">{user.name}</td>
             <td className="p-2"> {user.username} </td>
