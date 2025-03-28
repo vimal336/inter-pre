7e4eaea2c4664e11508d0edd50006d80343fdb08
vimal336 <asvimal336@gmail.com>
2025-03-28 17:59:15 +0530

    problem solving

--- src/TailwindSite/Header.jsx
+++ src/TailwindSite/Header.jsx
@@ -1,7 +1,10 @@
 const Header = () =>{
     return(
         <>
-        <h1>Header Section</h1>
+        <div className="bg-gray-500 h-5 flex ">
+         <img src="https://s.yimg.com/fz/api/res/1.2/ylU1uf_L90MwCB8EXPny4g--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05Njt3PTk2/https://tse4.mm.bing.net/th?q=Panda+Logo&pid=Api&mkt=en-US&cc=US&setlang=en&adlt=strict&t=1" alt="" />
+         <h1>Website</h1>
+        </div>
         </>
     )
 }
