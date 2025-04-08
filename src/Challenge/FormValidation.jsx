9d7d86d29a44c5272a2de526f29b518c56d48495
vimal336 <asvimal336@gmail.com>
2025-04-08 19:56:41 +0530

    problem solving

--- src/Challenge/FormValidation.jsx
+++ src/Challenge/FormValidation.jsx
@@ -87,7 +87,7 @@ export default function FormWithValidation() {
 
       <button
         type="submit"
-        className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-700"
+        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
       >
         Submit
       </button>
