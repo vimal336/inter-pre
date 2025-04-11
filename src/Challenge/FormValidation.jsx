135845dc1720f46833c4605eeb4d24b4869f94b1
vimal336 <asvimal336@gmail.com>
2025-04-11 17:26:26 +0530

    project

--- src/Challenge/FormValidation.jsx
+++ src/Challenge/FormValidation.jsx
@@ -87,7 +87,7 @@ export default function FormWithValidation() {
         type="submit"
         className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
       >
-        Submit
+        Submit 
       </button>
     </form>
   );
