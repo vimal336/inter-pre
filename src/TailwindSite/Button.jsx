9bd6279b1280c0bfb7fbdc10e461e88ecb29e526
vimal336 <asvimal336@gmail.com>
2025-04-14 15:43:20 +0530

    project

---
+++ src/TailwindSite/Button.jsx
@@ -0,0 +1,23 @@
+// components/Button.jsx
+import React from "react";
+
+const Button = ({
+  children,
+  onClick,
+  type = "button",
+  className = "",
+  disabled = false,
+}) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`px-4 py-2 rounded-md font-semibold text-white bg-blue-600 hover:bg-blue-700 disabled:bg-gray-400 transition ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/TailwindSite/Main.jsx
+++ src/TailwindSite/Main.jsx
@@ -1,12 +1,28 @@
-import Header from "./Header"
+//import Header from "./Header"
+import Button from "./Button";
 
 const Main = () =>{
-    return(
-        <>
-        <h1>Main Page</h1>
-        <Header/>
-        </>
-    )
-}
+    const handleClickOne = () => {
+        alert("Button One Clicked!");
+      };
+    
+      const handleClickTwo = () => {
+        console.log("Button Two Clicked!");
+      };
+    
+      return (
+        <div className="p-6 space-y-4">
+          <Button onClick={handleClickOne}>Click Me</Button>
+    
+          <Button onClick={handleClickTwo} className="bg-green-600 hover:bg-green-700">
+            Submit
+          </Button>
+    
+          <Button disabled className="bg-red-500">
+            Disabled
+          </Button>
+        </div>
+      );
+    };
 
 export default Main
