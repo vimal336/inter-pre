bd36f48cffce5e252e6609cca1b6728976ca1aa8
vimal336 <asvimal336@gmail.com>
2025-04-17 19:11:55 +0530

    problem solving

--- src/TailwindSite/Button.jsx
+++ src/TailwindSite/Button.jsx
@@ -13,3 +13,18 @@ const Button = ({children, onClick, type = "button", className = "", disabled = 
 };
 
 export default Button;
+
+const Button = ({children, onClick, type = "button", className = "", disabled = false,}) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`px-4 py-2 rounded-md font-semibold text-white bg-blue-600 hover:bg-blue-700 disabled:bg-gray-400 transition ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/TailwindSite/Header.jsx
+++ src/TailwindSite/Header.jsx
@@ -1,4 +1,4 @@
-const Header = () =>{
+const Header = () => {
     return(
         <>
         <div className="bg-gray-500 h-10 flex ">
