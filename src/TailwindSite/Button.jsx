c79823be7f16ab2a48b5e9a349909061ffd500e8
vimal336 <asvimal336@gmail.com>
2025-04-17 20:02:57 +0530

    problem solving

--- src/TailwindSite/Button.jsx
+++ src/TailwindSite/Button.jsx
@@ -18,7 +18,7 @@ const Button = ({ children, onClick, type = "button", className = "", disabled =
   return (
     <button
       type={type}
-      className={`bg-red-400 ${className}`}
+      className={`px-4 py-2 bg-red-400 ${className}`}
       onClick={onClick}
       disabled={disabled}
     >
