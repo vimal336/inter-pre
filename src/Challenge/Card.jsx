264484372fbca49b78f295e8a70e9bce66cadf40
vimal336 <asvimal336@gmail.com>
2025-05-08 22:16:37 +0530

    problem solving

--- src/Challenge/Card.jsx
+++ src/Challenge/Card.jsx
@@ -3,7 +3,9 @@ const Mycard = () =>{
 
     return(
         <>
-        
+        <div>
+            <img src="https://tse2.mm.bing.net/th?id=OIP.G37tgeQqSNt7v2oPfj9ltQHaE7&pid=Api&P=0&h=220" alt="" />
+        </div>
         
         </>
     )
