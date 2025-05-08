da02ddd82da3eacce21a9e1e69fd2605a11ae858
vimal336 <asvimal336@gmail.com>
2025-05-08 22:19:32 +0530

    problem solving

--- src/Challenge/Card.jsx
+++ src/Challenge/Card.jsx
@@ -1,25 +1,29 @@
-
-const Mycard = () =>{
-
-    return(
-        <>
+const Mycard = () => {
+  return (
+    <>
+      <div>
         <div>
-            <img src="https://tse2.mm.bing.net/th?id=OIP.G37tgeQqSNt7v2oPfj9ltQHaE7&pid=Api&P=0&h=220" alt="" />
+          <img
+            src="https://tse2.mm.bing.net/th?id=OIP.G37tgeQqSNt7v2oPfj9ltQHaE7&pid=Api&P=0&h=220"
+            alt=""
+          />
         </div>
 
         <div>
-            <h1>Card Heading </h1>
-            <span>$56</span>
+          <h1>Card Heading </h1>
+          <span>$56</span>
         </div>
 
         <div>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Sunt tempore quos rem accusamus voluptate. Blanditiis eaque eligendi nostrum distinctio ea.</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
+            tempore quos rem accusamus voluptate. Blanditiis eaque eligendi
+            nostrum distinctio ea.
+          </p>
         </div>
-        
-        </>
-    )
+      </div>
+    </>
+  );
+};
 
-}
-
-export default Mycard
+export default Mycard;
