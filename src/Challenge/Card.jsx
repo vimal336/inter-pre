b036709c23fc019bbc7956763f349ac9b7a89584
vimal336 <asvimal336@gmail.com>
2025-05-09 22:11:54 +0530

    problem solving

--- src/Challenge/Card.jsx
+++ src/Challenge/Card.jsx
@@ -34,13 +34,10 @@ const Mycard = () => {
     <>
       <div>
       <div className="flex justify-center items-center">
-        <div>
         <img
             src="https://tse2.mm.bing.net/th?id=OIP.G37tgeQqSNt7v2oPfj9ltQHaE7&pid=Api&P=0&h=220"
             alt=""
           />
-        </div>
-         
         </div>
 
         <div className="flex justify-between items-center">
