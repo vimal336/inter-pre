de0eb176e55289d8f2d53f13f179f63dc6cd125b
vimal336 <asvimal336@gmail.com>
2025-05-15 22:59:01 +0530

    problem solving

--- src/Challenge/FormValidation.jsx
+++ src/Challenge/FormValidation.jsx
@@ -162,7 +162,7 @@ const FormValidation = () => {
 
       <button
         type="submit"
-        className="bg-blue-400 text-white px-4 py-2 rounded hover:bg-gray-400"
+        className="bg-blue-400 text-white px-4 py-2 rounded hover:bg-gray-600"
       >
         Submit
       </button>
