b8067213c388d147b01d148278a3393c12c236be
vimal336 <asvimal336@gmail.com>
2025-06-09 22:12:38 +0530

    problem solving

--- src/Challenge/FormValidation.jsx
+++ src/Challenge/FormValidation.jsx
@@ -142,7 +142,7 @@ const FormValidation = () => {
     onChange={handleChange}
     />
     <button onSubmit={handleSubmit} type="submit" className="bg-red-600 mt-2">
-          Submit.
+          Submit
         </button>
     </div>
     </form> 
