ccc4787e2b139160834c25acb17f52a0de2280f5
vimal336 <asvimal336@gmail.com>
2025-07-10 21:40:27 +0530

    problem solving

--- src/TailwindSite/Header.jsx
+++ src/TailwindSite/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
          <h1>Website heading</h1>
         </div>
         <div className="flex h-[12rem] bg-red-400">
-            <h1>WEb developer</h1>
+            <h1>Web developer</h1>
         </div>
         </>
     )
