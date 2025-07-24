1359417fcf9bfffc764f7b8c3269b153463e3714
vimal336 <asvimal336@gmail.com>
2025-07-24 20:24:28 +0530

    problem solving

--- src/Challenge/TableFilter.jsx
+++ src/Challenge/TableFilter.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const TableFilter = () => {
   const [fetchData, setFetchData] = useState([])
 
-  const fetch = 
+  const data = fetch
   return( 
   <>
   <h1>Table data</h1>
