b97ef4dd90dc88a0b55927245ba39aa565ebd0f2
vimal336 <asvimal336@gmail.com>
2025-08-04 21:05:14 +0530

    problem solving

--- src/Challenge/TableFilter.jsx
+++ src/Challenge/TableFilter.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const TableFilter = () => {
   const [fetchData, setFetchData] = useState([])
 
-  const data = fetch("https://jsonplaceholder.typicode.com/posts/1").then(((res)=>(res.json())));
+  const data = fetch("https://jsonplaceholder.typicode.com/posts").then(((res)=>(res.json())));
 
   console.log(data)
 
