e7cb2c4caf2620160d0c451b08e2a3801fe29755
vimal336 <asvimal336@gmail.com>
2025-09-17 22:17:07 +0530

    problem solving

--- src/Challenge/TableFilter.jsx
+++ src/Challenge/TableFilter.jsx
@@ -4,7 +4,7 @@ const Tablefilter = () => {
   const [data, tableData] = useState([])
 
   useEffect(()=>{
-     const url = "https://jsonplaceholder.typicode.com/posts".then((url.json()))
+     const url = "https://jsonplaceholder.typicode.com/posts".then((url.json()).then((res)))
   },[])
 
   const info = [
