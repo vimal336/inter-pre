3ebd3185e5b4f06123d2a42f5b401d995bc68ffb
vimal336 <asvimal336@gmail.com>
2025-10-07 20:50:36 +0530

    problem solving

--- src/Challenge/TableFilter.jsx
+++ src/Challenge/TableFilter.jsx
@@ -8,7 +8,7 @@ const Tablefilter = () => {
   },[])
 
   const information = [
-     {name: 'ravi ram', age:'42', },
+     {name: 'ravi ram', age:'40', },
           {name: 'raj', age:'24'},
                {name: 'ram', age:'22'},
   ]
