16133ca6a07236469c6746b8e34be89972d0ea91
vimal336 <asvimal336@gmail.com>
2025-10-27 15:31:24 +0530

    problem solving

--- src/Challenge/TableFilter.jsx
+++ src/Challenge/TableFilter.jsx
@@ -70,25 +70,27 @@ import { useState, useEffect } from "react";
 
 const FetchData = async () => {
 
-const [data, setData] = useState([]);
-const [loading, setLoading] = useState(true);
-const [error, setError] = useState([]);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState([]);
 
 
-   try{
-const url = fetch("https://jsonplaceholder.typicode.com/posts");
-const response = await url.json();
-setData(response)
-   }catch(err){
+  try {
+    setLoading(true)
+    const url = fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await url.json();
+    setData(response)
+  } catch (err) {
 
-   }
-   finally{
+  }
+  finally {
+    setLoading(false)
+  }
 
-   }
+  useEffect(() => {
 
-   useEffect(()=>{
-
-   },[])
+    FetchData();
+  }, [])
 
 }
 
