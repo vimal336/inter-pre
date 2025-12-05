dfbf839582ca0e13fb0a6a8d47bf04567d27c2af
vimal336 <asvimal336@gmail.com>
2025-12-05 10:27:37 +0530

    async

--- src/Challenge/TanstackQuery.jsx
+++ src/Challenge/TanstackQuery.jsx
@@ -22,7 +22,7 @@ const TanstackQuery = () => {
     },
   });
 
-  if (isLoading) return <p>Loading....</p>;
+  if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error fetching data</p>;
 
   return (
