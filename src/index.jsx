5f06ed748407d6475c06eb0500718cdc9785a4a1
vimal336 <asvimal336@gmail.com>
2025-12-06 22:16:17 +0530

    tanstack

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient(); // <-- create client
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+   <App/>
   </React.StrictMode>
 );
 
