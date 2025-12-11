cf1432f033b843c3825335c2bdcf1b08e4194247
vimal336 <asvimal336@gmail.com>
2025-12-11 19:39:08 +0530

    tanstack

--- src/Challenge/TableFilter.jsx
+++ src/Challenge/TableFilter.jsx
@@ -71,7 +71,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
       <h1 className="text-2xl font-semibold mb-4 text-gray-800">
-        Simple Search using tanstack query.
+        Simple Search using tanstack query
       </h1>
 
       <div className="w-full max-w-md">
