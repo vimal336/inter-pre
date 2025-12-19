4196b6fab662db230e5f729d43a4303a47b9e6dd
vimal336 <asvimal336@gmail.com>
2025-12-19 11:40:19 +0530

    tanstack

--- src/Challenge/FormValidation.jsx
+++ src/Challenge/FormValidation.jsx
@@ -134,7 +134,7 @@ const FormValidation = () => {
     value={FormData.name}
     onChange={handleChange}
    />
-    <label htmlFor=""> Age:</label>
+    <label htmlFor=""> Age</label>
     <input 
     type="number" 
     name="age" 
