907f26a149e31d5bd75f7a1fe0b70cff422ec365
vimal336 <asvimal336@gmail.com>
2025-12-29 10:24:02 +0530

    tanstack

--- src/Challenge/FormValidation.jsx
+++ src/Challenge/FormValidation.jsx
@@ -139,7 +139,7 @@ const FormValidation = () => {
     value={FormData.age}
     onChange={handleChange}
     />
-    <button onSubmit={handleSubmit} type="submit" className="bg-red-200 mt-2">
+    <button onSubmit={handleSubmit} type="submit" className="bg-red-200 mt-4">
           Submit.
         </button>
     </div>
