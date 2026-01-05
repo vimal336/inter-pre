4c3110c7a0e27c88d0d6466ad07f8e15c8c7dc6a
vimal336 <asvimal336@gmail.com>
2026-01-05 10:03:15 +0530

    tanstack

--- src/Challenge/TableFilter.jsx
+++ src/Challenge/TableFilter.jsx
@@ -97,7 +97,7 @@ export default function App() {
               </li>
             ))
           ) : (
-            <li className="text-gray-400 text-center">No data found</li>
+            <li className="text-gray-400 text-center">No results found</li>
           )}
         </ul>
       </div>
