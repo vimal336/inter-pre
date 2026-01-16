1adcdd98dbb8b366bcea098d9d2c1dbc72425261
vimal336 <asvimal336@gmail.com>
2026-01-16 22:05:57 +0530

    tanstack

--- src/Challenge/Mytask.jsx
+++ src/Challenge/Mytask.jsx
@@ -148,7 +148,7 @@ const App = () => {
             <tr className="text-center">
             <th>Name</th>
             <th> Username</th>
-            <th>Email:</th>
+            <th>Email</th>
             </tr>
         </thead>
         {
